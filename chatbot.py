fc9de43a80c33e92d670707c828a194b9c36dcad
Sanjay Lalwani <sanjay.lalwani3@gmail.com>
2025-01-13 14:23:04 +0530

    Add files via upload

--- chatbot.py
+++ chatbot.py
@@ -2,6 +2,7 @@ import os
 import json
 import logging
 from groq import Groq
+import streamlit as st
 from qdrant_client import QdrantClient, models
 from sentence_transformers import SentenceTransformer
 from langchain_groq import ChatGroq
@@ -12,27 +13,50 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
-# Initialize Groq LLM
-llm = ChatGroq(
-    model_name="llama-3.3-70b-versatile",
-    temperature=0.7
-)
+st.title("AI Research Bot")
 
+# Initialize chat history
+if "messages" not in st.session_state:
+    with st.chat_message("user"):
+        st.write("Hello 👋")
+    st.session_state.messages = []
 
-# Create a simple prompt
-prompt = ChatPromptTemplate.from_messages([
-    ("system", "You are chatbot which help to understand the current trends in research from different AI areas like Machine Learning, Computer Vision, NLP."),
-    ("user", "{input}")
-])
-
-# Create the chain that guarantees JSON output
-chain = prompt | llm 
-def parse_product(description: str) -> dict:
-    result = chain.invoke({"input": description})
-    print(result)
-
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
         
-# Example usage
-description = """Text from Qdrant vector database."""
+input = st.chat_input("Say something")
+if input:
+    # Initialize Groq LLM
+    llm = ChatGroq(
+        model_name="llama-3.3-70b-versatile",
+        temperature=0.7)
 
-parse_product(description)
+    # Create a simple prompt
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", "You are chatbot which help to understand the current trends in research from different AI areas like Machine Learning, Computer Vision, NLP."),
+        ("user", "{input}")
+    ])
+
+    # Create the chain that guarantees JSON output
+    chain = prompt | llm 
+    def parse_product(description: str) -> dict:
+        result = chain.invoke({"input": description})
+        return result
+            
+    # Example usage
+    description = prompt # """Text from Qdrant vector database."""
+
+    output = parse_product(input)
+
+    # Display user message in chat message container
+    st.chat_message("user").markdown(input)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": input})
+
+    response = f"Echo: {prompt}"
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        st.markdown(output.content)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": output.content})
