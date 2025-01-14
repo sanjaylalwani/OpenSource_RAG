8e4c085f2eafc1e90b53c324ad6948485a1c9fc4
Sanjay Lalwani <sanjay.lalwani3@gmail.com>
2025-01-14 21:57:23 +0530

    Add files via upload

--- chatbot.py
+++ chatbot.py
@@ -13,8 +13,12 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
-st.title("AI Research Bot")
+qdrant_client = QdrantClient(url=os.getenv("QDRANT_URL"), 
+                            api_key=os.getenv("QDRANT_API_KEY"),)
 
+encoder = SentenceTransformer('all-MiniLM-L6-v2')
+
+st.title("AI Research Bot")
 # Initialize chat history
 if "messages" not in st.session_state:
     with st.chat_message("user"):
@@ -27,6 +31,19 @@ for message in st.session_state.messages:
         
 input = st.chat_input("Say something")
 if input:
+
+    hits = qdrant_client.search(
+        collection_name="rag_osc",
+        query_vector=encoder.encode(input),
+        limit=3,)
+
+    qdrnt_response = ""
+    for hit in hits:
+        qdrnt_response += "Document Text: " + hit.payload['abstract'] + "  \n"
+
+
+    updated_prompt = "Please give precise answer to the question "+ input + " this is context: " + qdrnt_response 
+
     # Initialize Groq LLM
     llm = ChatGroq(
         model_name="llama-3.3-70b-versatile",
@@ -35,28 +52,29 @@ if input:
     # Create a simple prompt
     prompt = ChatPromptTemplate.from_messages([
         ("system", "You are chatbot which help to understand the current trends in research from different AI areas like Machine Learning, Computer Vision, NLP."),
-        ("user", "{input}")
+        ("human","{bot_input}")
     ])
 
     # Create the chain that guarantees JSON output
     chain = prompt | llm 
     def parse_product(description: str) -> dict:
-        result = chain.invoke({"input": description})
+        result = chain.invoke({"bot_input": description})
         return result
             
     # Example usage
     description = prompt # """Text from Qdrant vector database."""
 
-    output = parse_product(input)
+    output = parse_product(updated_prompt)
 
     # Display user message in chat message container
     st.chat_message("user").markdown(input)
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": input})
 
-    response = f"Echo: {prompt}"
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": updated_prompt})
+
     # Display assistant response in chat message container
     with st.chat_message("assistant"):
         st.markdown(output.content)
+    
     # Add assistant response to chat history
     st.session_state.messages.append({"role": "assistant", "content": output.content})
